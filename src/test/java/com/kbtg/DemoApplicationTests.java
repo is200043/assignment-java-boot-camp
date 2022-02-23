cd7b38032d9eda9f5426368568cdf6416a6111c6
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2022-02-23 19:14:31 +0700

    comment test

--- src/test/java/com/kbtg/DemoApplicationTests.java
+++ src/test/java/com/kbtg/DemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
